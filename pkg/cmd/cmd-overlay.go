990d73fa89b76176f9c72e7c52a067d660888eb5
Arnaud Lemaire <alemaire@norad.fr>
2026-07-23 01:58:43 +0200

    feat(overlay): add strip flag to merge parents folders

--- pkg/cmd/cmd-overlay.go
+++ pkg/cmd/cmd-overlay.go
@@ -7,18 +7,21 @@ import (
 )
 
 func OverlayCommand() *cobra.Command {
+	var stripDepth int
 	cmd := &cobra.Command{
 		Use:   "overlay path target",
 		Short: "Replicate the same files structure without integrity in filenames",
 		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			directory := integrity.Path{
-				Regex:     fim.FIM.Regex,
-				Inclusive: fim.FIM.PatternIsInclusive,
-				Strategy:  integrity.NewStrategy(fim.FIM.Strategy, fim.FIM.Hash),
+				Regex:      fim.FIM.Regex,
+				Inclusive:  fim.FIM.PatternIsInclusive,
+				Strategy:   integrity.NewStrategy(fim.FIM.Strategy, fim.FIM.Hash),
+				StripDepth: stripDepth,
 			}
 			return directory.Overlay(args[0], args[1])
 		},
 	}
+	cmd.Flags().IntVarP(&stripDepth, "strip", "s", 0, "number of top-level parent folders to strip away (0 = keep full structure)")
 	return cmd
 }
